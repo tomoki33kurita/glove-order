2409fa94d7f7de2c8732357a3029c835850fd20e
tomoki33kurita <tomoki33kurita@gmail.com>
2021-03-16 23:11:40 +0900

    Second commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.node = {
+//         puppeteer: 'empty',
+//         child_process: 'empty',
+//         fs: 'empty',
+//         net: 'empty',
+//         dns: 'empty',
+//         tls: 'empty',
+//         readline: 'empty',
+//       }
+//     }
+
+//     return config
+//   },
+// }
