ebc5b74a08ce900434eb1178857045e22b488109
tomoki33kurita <tomoki33kurita@gmail.com>
2021-06-12 10:03:24 +0900

    Added analysis page in page/

---
+++ src/pages/analysis.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from '@material-ui/core'
+
+const Analysis: React.VFC<{}> = ({}) => {
+  return (
+    <Box mt={2} ml={2}>
+      <Box>ここは分析をするためのページであるのだ</Box>
+    </Box>
+  )
+}
+
+export default Analysis
