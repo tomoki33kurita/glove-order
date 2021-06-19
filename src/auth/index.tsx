941a241cde2aab197c9f4c6827180c66512b6de8
tomoki33kurita <tomoki33kurita@gmail.com>
2021-06-19 16:06:20 +0900

    Implemented auth listen system

--- src/auth/index.tsx
+++ src/auth/index.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { Box } from '@material-ui/core'
 import { auth } from 'src/firebase'
 import { useRouter } from 'next/router'
 
 const Auth: React.FC<{}> = ({ children }) => {
-  // ページへのアクセス者が、ログイン済みかどうかチェック
   const router = useRouter()
   React.useEffect(() => {
-    !auth.currentUser && router.push('/signIn')
+    auth.onAuthStateChanged((user) => {
+      if (!user) {
+        router.push('/signIn')
+      }
+    })
   }, [])
-
-  return (
-    <>
-      <Box>{children}</Box>
-    </>
-  )
+  return <>{children}</>
 }
 
 export default Auth
--- src/pages/analysis.tsx
+++ src/pages/analysis.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
-import { Box } from '@material-ui/core'
+import { Box, CircularProgress } from '@material-ui/core'
 import Auth from 'src/auth'
+import { auth } from 'src/firebase'
 
 const Analysis: React.VFC<{}> = ({}) => {
+  const [user, setUser] = React.useState<any>(false)
+  React.useEffect(() => {
+    auth.onAuthStateChanged((user) => setUser(user))
+  }, [])
+
   return (
     <Auth>
-      <Box mt={2} ml={2}>
-        <Box>ここは分析をするためのページであるのだ</Box>
-      </Box>
+      {user ? (
+        <Box mt={2} ml={2}>
+          <Box>ここは分析をするためのページであるのだ</Box>
+          <Box>ユーザー：{user?.email}</Box>
+        </Box>
+      ) : (
+        <Box style={{ position: 'absolute', top: '50%', left: '50%' }}>
+          <CircularProgress />
+        </Box>
+      )}
     </Auth>
   )
 }
